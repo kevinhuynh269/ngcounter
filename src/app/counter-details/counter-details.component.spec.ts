5b77204643f07a379eb4767c2bce25e27a467384
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-04-21 10:28:25 -0400

    testing extended for super counter implementation

--- src/app/counter-details/counter-details.component.spec.ts
+++ src/app/counter-details/counter-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CounterComponent } from '../counter/counter.component';
 import { SuperCounterComponent } from '../super-counter/super-counter.component';
@@ -47,6 +47,7 @@ describe('CounterDetailsComponent', () => {
   });
 
   it('handles value iteration', () => {
+    //handle normal counters
     component.increaseValue();
     component.increaseValue();
 
@@ -55,17 +56,46 @@ describe('CounterDetailsComponent', () => {
     component.decreaseValue();
 
     expect(component.getValue()).toEqual(1);
+
+    //handle super counters
+    addSuperCounter();
+    component.increaseValue();
+    component.increaseValue();
+    expect(component.getValue()).toEqual(6);
+
+    component.decreaseValue();
+    expect(component.getValue()).toEqual(3);
   })
+
+  const isDecreaseDisabled = () => {
+    return fixture.nativeElement.querySelector(".decrease").disabled;
+  }
 
   it("button disables when value becomes 0 on normal counter", () => {
     //when value is at 0, button should be disabled
     expect(component.getValue()).toEqual(0);
-    expect(fixture.nativeElement.querySelector(".decrease").disabled).toBeTruthy();
+    expect(isDecreaseDisabled()).toBeTruthy();
 
     //increase value which enables the button
     component.increaseValue();
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector(".decrease").disabled).toBeFalsy();
+    expect(isDecreaseDisabled()).toBeFalsy();
+
+    //handle super counters
+    addSuperCounter();
+    superCounter.setValue(2);
+
+    fixture.detectChanges();
+
+    expect(isDecreaseDisabled()).toBeTruthy();
+
+    //increase value so button should reenable
+    component.increaseValue();
+
+    fixture.detectChanges();
+
+    expect(isDecreaseDisabled()).toBeFalsy();
+
   })
 
   it("checks if the message is correct", () => {
